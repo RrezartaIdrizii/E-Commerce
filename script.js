8a9965896ed570e931b853b375c891d64511556b
RrezartaIdrizii <104738075+RrezartaIdrizii@users.noreply.github.com>
2023-06-13 01:36:16 +0200

    Implementing some things

--- script.js
+++ script.js
@@ -32,3 +32,69 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setTimeout(revealElements, 1000);
   });
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    const playButton = document.getElementById('playButton');
+    const stopButton = document.getElementById('stopButton');
+    const audio = document.getElementById('audio');
+  
+    playButton.addEventListener('click', () => {
+      audio.play();
+    });
+  
+    stopButton.addEventListener('click', () => {
+      audio.pause();
+      audio.currentTime = 0;
+    });
+  });
+     
+
+
+  const slider = document.querySelector('.slider');
+const images = [
+  'img/image1.jpg',
+  'img/image2.jpg',
+  'img/image3.jpg',
+  'img/image1.jpg',
+  'img/image2.jpg',
+  'img/image3.jpg'
+];
+let slideIndex = 0;
+let isTransitioning = false;
+
+function showSlide() {
+  const startIndex = slideIndex * 3;
+  const endIndex = startIndex + 3;
+  const slideImages = images.slice(startIndex, endIndex);
+
+  slider.innerHTML = '';
+
+  for (let i = 0; i < slideImages.length; i++) {
+    const img = document.createElement('img');
+    img.src = slideImages[i];
+    img.alt = `Photo ${i + 1}`;
+    slider.appendChild(img);
+  }
+
+  slider.classList.add('slide-transition');
+  setTimeout(() => {
+    slider.classList.remove('slide-transition');
+    isTransitioning = false;
+  }, 300);
+}
+function showNextSlide() {
+  if (isTransitioning) return;
+
+  slideIndex++;
+  if (slideIndex >= Math.ceil(images.length / 3)) {
+    slideIndex = 0;
+  }
+  isTransitioning = true;
+  showSlide();
+}
+
+
+slider.addEventListener('click', showNextSlide);
+
+// Show the initial set of images
+showSlide();
